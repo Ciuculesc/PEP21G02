87eb44d95287251297ddbaa09a66273aa5799fc8
ciucules <oana_larisa96@yahoo.com>
2021-04-14 21:27:49 +0300

    add modeul 1

---
+++ modul1.py
@@ -0,0 +1,30 @@
+print('Hello World')
+print('print\u200b("")')
+# print​("")
+HELLO_WORLD = "Hello World"
+print(HELLO_WORLD)
+
+name = 'OANA'
+print(3 * name)
+# print(3 * (name+ ' ' ))
+print(name, name, name, sep='#', end=' ')
+
+print(100 * (name + '\n'))
+
+name1 = 'OAMA2\n'
+print(100 * name1)
+
+print(print)
+
+a = '_'
+print(5 * a)
+print('\   /')
+print( ' ''\ /')
+print('  ''.')
+
+b='--'
+print(2*b)
+c='|'
+print('|   |')
+print('|   |')
+print(2*b)
